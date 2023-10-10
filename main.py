72ab5a799e4368d7f7201922a441a3369d61d2b2
master-saz <sebastian.andrade@ithinkupc.com>
2023-10-10 14:58:02 +0200

    test

---
+++ main.py
@@ -0,0 +1,7 @@
+def print_hi():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi()
